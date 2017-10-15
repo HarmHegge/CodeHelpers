8e823e3867eaf50ec4b556b1f04f9a60fb3c6065
Harm Hegge <harmsimon@heggeweb.nl>
2017-10-15 12:02:01 +0200

    Added method helpers

---
+++ MethodHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CodeHelpers
+{
+    public static class MethodHelper
+    {
+        public static void ThrowIfNull<T>(T value, string valueName) where T:class
+        {
+            if(value == null)
+            {
+                throw new ArgumentNullException(valueName);
+            }
+        }
+
+        public static void NotNull<T>(this T value, string valueName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(valueName);
+            }
+        }
+    }
+}
